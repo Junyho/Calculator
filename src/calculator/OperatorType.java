be19a05c60d9a5bc5efb5653a311e171d613a7fa
junyho <inop159@naver.com>
2026-06-17 11:04:37 +0900

    feat: OperatorType enum으로 사칙연산 처리 구현

---
+++ src/calculator/OperatorType.java
@@ -0,0 +1,44 @@
+package calculator;
+
+public enum OperatorType {
+
+
+    PLUS('+') {
+        @Override
+        public double operate(double num1, double num2) {
+            return num1 + num2;
+        }
+    },
+    MINUS('-') {
+        @Override
+        public double operate(double num1, double num2) {
+            return num1 - num2;
+        }
+    },
+    MULTIPLY('*') {
+        @Override
+        public double operate(double num1, double num2) {
+            return num1 * num2;
+        }
+    },
+    DIVIDE('/') {
+        @Override
+        public double operate(double num1, double num2) {
+            if (num2 == 0) {
+                throw new ArithmeticException("0으로 나눌 수 없습니다.");
+            }
+            return num1 / num2;
+        }
+    };
+
+    private final char type;
+
+    OperatorType(char type) {
+        this.type = type;
+    }
+
+
+    public abstract double operate(double num1, double num2);
+
+
+}
